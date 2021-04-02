3332d9a22c4a05803920892a77050819e8c080a7
unknown <sinumsd98@gmail.com>
2021-04-02 11:59:42 +0530

    ADDED: calculate total emp working

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -11,3 +11,43 @@ then
 else
      echo "Employee is Absent"
 fi
+totalEmpHr=0
+totalWorkingDays=0
+function getWorkingHours(){
+
+while [[ $totalEmpHr -le $max_Hrs_in_month  &&  $totalWorkingDays -le $num_Working_Days ]]
+do
+    empHrs=0
+    (( totalWorkingDays++ ))
+     empCheck=$((RANDOM%3))
+
+      case $empCheck in
+      case $1 in
+        $isFullTime)
+               empHrs=8
+               workingHours=8
+                 ;;
+        $isPartTime)
+               empHrs=4
+               workingHours=4
+                 ;;
+                 *)
+               empHrs=0
+               workingHours=0
+                 ;;
+      esac
+      totalEmpHr=$(( $totalEmpHr+$empHrs ))
+      echo $workingHours
+}
+while [[ $totalWorkingHours -le $max_Hrs_in_month  &&  $totalWorkingDays -le $num_Working_Days ]]
+do
+
+    (( totalWorkingDays++ ))
+     workingHours="$( getWorkingHours $((RANDOM%3)) )"
+
+
+      totalWorkingHours=$(( $totalWorkingHours + $workingHours ))
+
+done
+totalSalary=$(( $totalEmpHr * $empRatePerHr ))
+totalSalary=$(( $totalWorkingHours * $empRatePerHr ))
